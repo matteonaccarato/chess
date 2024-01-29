09195e878ff7c5b57b39626b197cea39fe5dc75d
Matteo Naccarato <matteo.naccarato@itiscuneo.eu>
2024-01-29 12:50:59 +0100

    First commit

---
+++ Source/chess/chessGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "chessGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHESS_API AchessGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
